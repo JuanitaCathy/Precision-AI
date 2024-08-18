4e23168e5e709321ca885e30ec0ea6b687be5a81
SyedaFarheen1 <spongheen@gmail.com>
2024-08-18 23:22:46 +0500

    Made the Product Details Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,20 @@
-import HeroCarousel from "@/components/HeroCarousel"
-import Searchbar from "@/components/Searchbar"
-import Image from "next/image"
-// import { getAllProducts } from "@/lib/actions"
-// import ProductCard from "@/components/ProductCard"
+import HeroCarousel from "@/components/HeroCarousel";
+import Searchbar from "@/components/Searchbar";
+import Image from "next/image";
+import { getAllProducts } from "@/lib/actions";
+import ProductCard from "@/components/ProductCard";
 
 const Home = async () => {
-  // const allProducts = await getAllProducts();
+  const allProducts = await getAllProducts();
 
   return (
     <>
       <section className="px-6 md:px-20 py-24">
         <div className="flex max-xl:flex-col gap-16">
-          <div className="flex flex-col justify-center"> 
+          <div className="flex flex-col justify-center">
             <p className="small-text">
               Smart Shopping Starts Here:
-              <Image 
+              <Image
                 src="/assets/icons/arrow-right.svg"
                 alt="arrow-right"
                 width={16}
@@ -28,7 +28,8 @@ const Home = async () => {
             </h1>
 
             <p className="mt-6">
-              Powerful, self-serve product and growth analytics to help you convert, engage, and retain more.
+              Powerful, self-serve product and growth analytics to help you
+              convert, engage, and retain more.
             </p>
 
             <Searchbar />
@@ -41,14 +42,14 @@ const Home = async () => {
       <section className="trending-section">
         <h2 className="section-text">Trending</h2>
 
-        {/* <div className="flex flex-wrap gap-x-8 gap-y-16">
+        <div className="flex flex-wrap gap-x-8 gap-y-16">
           {allProducts?.map((product) => (
             <ProductCard key={product._id} product={product} />
           ))}
-        </div> */}
+        </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
